a3ba5de29688bfeb6b888ff0533c1dbf426acb82
Aaron Dean <8dino2@gmail.com>
2024-09-10 14:33:06 -0400

    Seems to be working better now?

--- src/common/msg.c
+++ src/common/msg.c
@@ -2716,7 +2716,7 @@ void MSG_ParseDeltaPlayerstate_Default(const player_state_t *from,
 
 /*
 ===================
-MSG_ParseDeltaPlayerstate_Default
+MSG_ParseDeltaPlayerstate_Enhanced
 ===================
 */
 void MSG_ParseDeltaPlayerstate_Enhanced(const player_state_t    *from,
@@ -2854,50 +2854,91 @@ void MSG_ParseDeltaPlayerstate_Enhanced(const player_state_t    *from,
         MSG_ReadStats(to, psflags);
 }
 
-
-
+/*
+===================
+MSG_ParseDeltaPlayerstate_Aqtion
+===================
+*/
 void MSG_ParseDeltaPlayerstate_Aqtion(const player_state_t    *from,
-	player_state_t    *to,
-	int               flags,
-	int               extraflags,
-    msgPsFlags_t      psflags)
+                                        player_state_t          *to,
+                                        int                     flags,
+                                        int                     extraflags,
+                                        msgPsFlags_t            psflags)
 {
-	int         i;
-	int         statbits;
-	int			aqtflags;
+    Q_assert(to);
 
-	Q_assert(to);
+    int			aqtflags;
 
-	// clear to old value before delta parsing
-	if (!from) {
-		memset(to, 0, sizeof(*to));
-	} else if (to != from) {
-		memcpy(to, from, sizeof(*to));
-	}
+    // clear to old value before delta parsing
+    if (!from) {
+        memset(to, 0, sizeof(*to));
+    } else if (to != from) {
+        memcpy(to, from, sizeof(*to));
+    }
 
-	//
-	// parse the pmove_state_t
-	//
-	if (flags & PS_M_TYPE)
-		to->pmove.pm_type = MSG_ReadByte();
+    //
+    // parse the pmove_state_t
+    //
+    if (flags & PS_M_TYPE)
+        to->pmove.pm_type = MSG_ReadByte();
 
-	if (flags & PS_M_ORIGIN) {
-		to->pmove.origin[0] = MSG_ReadShort();
-		to->pmove.origin[1] = MSG_ReadShort();
-	}
+    if (psflags & MSG_PS_EXTENSIONS_2) {
+        if (flags & PS_M_ORIGIN) {
+            MSG_ReadDeltaInt23(&to->pmove.origin[0]);
+            MSG_ReadDeltaInt23(&to->pmove.origin[1]);
+        }
 
-	if (flags & PS_M_GRAVITY)
-		to->pmove.gravity = MSG_ReadShort();
+        if (extraflags & EPS_M_ORIGIN2)
+            MSG_ReadDeltaInt23(&to->pmove.origin[2]);
 
-	if (flags & PS_M_DELTA_ANGLES) {
-		to->pmove.delta_angles[0] = MSG_ReadShort();
-		to->pmove.delta_angles[1] = MSG_ReadShort();
-		to->pmove.delta_angles[2] = MSG_ReadShort();
-	}
+        if (flags & PS_M_VELOCITY) {
+            MSG_ReadDeltaInt23(&to->pmove.velocity[0]);
+            MSG_ReadDeltaInt23(&to->pmove.velocity[1]);
+        }
 
-	//
-	// parse the aqtion extensions
-	//
+        if (extraflags & EPS_M_VELOCITY2)
+            MSG_ReadDeltaInt23(&to->pmove.velocity[2]);
+
+        if (flags & PS_M_TIME)
+            to->pmove.pm_time = MSG_ReadWord();
+
+        if (flags & PS_M_FLAGS)
+            to->pmove.pm_flags = MSG_ReadWord();
+    } else {
+        if (flags & PS_M_ORIGIN) {
+            to->pmove.origin[0] = MSG_ReadShort();
+            to->pmove.origin[1] = MSG_ReadShort();
+        }
+
+        if (extraflags & EPS_M_ORIGIN2)
+            to->pmove.origin[2] = MSG_ReadShort();
+
+        if (flags & PS_M_VELOCITY) {
+            to->pmove.velocity[0] = MSG_ReadShort();
+            to->pmove.velocity[1] = MSG_ReadShort();
+        }
+
+        if (extraflags & EPS_M_VELOCITY2)
+            to->pmove.velocity[2] = MSG_ReadShort();
+
+        if (flags & PS_M_TIME)
+            to->pmove.pm_time = MSG_ReadByte();
+
+        if (flags & PS_M_FLAGS)
+            to->pmove.pm_flags = MSG_ReadByte();
+    }
+
+    if (flags & PS_M_GRAVITY)
+        to->pmove.gravity = MSG_ReadShort();
+
+    if (flags & PS_M_DELTA_ANGLES) {
+        to->pmove.delta_angles[0] = MSG_ReadShort();
+        to->pmove.delta_angles[1] = MSG_ReadShort();
+        to->pmove.delta_angles[2] = MSG_ReadShort();
+    }
+    //
+    // parse the aqtion extensions
+    //
 #if AQTION_EXTENSION
 	aqtflags = MSG_ReadByte();
 
@@ -2922,80 +2963,64 @@ void MSG_ParseDeltaPlayerstate_Aqtion(const player_state_t    *from,
 		MSG_ReadByte();
 #endif
 
+    //
+    // parse the rest of the player_state_t
+    //
+    if (flags & PS_VIEWOFFSET) {
+        to->viewoffset[0] = MSG_ReadChar() * 0.25f;
+        to->viewoffset[1] = MSG_ReadChar() * 0.25f;
+        to->viewoffset[2] = MSG_ReadChar() * 0.25f;
+    }
 
+    if (flags & PS_VIEWANGLES) {
+        to->viewangles[0] = MSG_ReadAngle16();
+        to->viewangles[1] = MSG_ReadAngle16();
+    }
 
-	//
-	// parse the rest of the player_state_t
-	//
-	if (flags & PS_VIEWOFFSET) {
-		to->viewoffset[0] = MSG_ReadChar() * 0.25f;
-		to->viewoffset[1] = MSG_ReadChar() * 0.25f;
-		to->viewoffset[2] = MSG_ReadChar() * 0.25f;
-	}
+    if (extraflags & EPS_VIEWANGLE2)
+        to->viewangles[2] = MSG_ReadAngle16();
 
-	if (flags & PS_VIEWANGLES) {
-		to->viewangles[0] = MSG_ReadAngle16();
-		to->viewangles[1] = MSG_ReadAngle16();
-	}
+    if (flags & PS_KICKANGLES) {
+        to->kick_angles[0] = MSG_ReadChar() * 0.25f;
+        to->kick_angles[1] = MSG_ReadChar() * 0.25f;
+        to->kick_angles[2] = MSG_ReadChar() * 0.25f;
+    }
 
-	if (extraflags & EPS_VIEWANGLE2) {
-		to->viewangles[2] = MSG_ReadAngle16();
-	}
-
-	if (flags & PS_KICKANGLES) {
-		to->kick_angles[0] = MSG_ReadChar() * 0.25f;
-		to->kick_angles[1] = MSG_ReadChar() * 0.25f;
-		to->kick_angles[2] = MSG_ReadChar() * 0.25f;
-	}
-
-	if (flags & PS_WEAPONINDEX) {
+    if (flags & PS_WEAPONINDEX) {
         if (psflags & MSG_PS_EXTENSIONS)
             to->gunindex = MSG_ReadWord();
         else
             to->gunindex = MSG_ReadByte();
     }
 
-	if (flags & PS_WEAPONFRAME) {
-		to->gunframe = MSG_ReadByte();
-	}
+    if (flags & PS_WEAPONFRAME)
+        to->gunframe = MSG_ReadByte();
 
-	if (extraflags & EPS_GUNOFFSET) {
-		to->gunoffset[0] = MSG_ReadChar() * 0.25f;
-		to->gunoffset[1] = MSG_ReadChar() * 0.25f;
-		to->gunoffset[2] = MSG_ReadChar() * 0.25f;
-	}
+    if (extraflags & EPS_GUNOFFSET) {
+        to->gunoffset[0] = MSG_ReadChar() * 0.25f;
+        to->gunoffset[1] = MSG_ReadChar() * 0.25f;
+        to->gunoffset[2] = MSG_ReadChar() * 0.25f;
+    }
 
-	if (extraflags & EPS_GUNANGLES) {
-		to->gunangles[0] = MSG_ReadChar() * 0.25f;
-		to->gunangles[1] = MSG_ReadChar() * 0.25f;
-		to->gunangles[2] = MSG_ReadChar() * 0.25f;
-	}
+    if (extraflags & EPS_GUNANGLES) {
+        to->gunangles[0] = MSG_ReadChar() * 0.25f;
+        to->gunangles[1] = MSG_ReadChar() * 0.25f;
+        to->gunangles[2] = MSG_ReadChar() * 0.25f;
+    }
 
-	if (flags & PS_BLEND) {
-		to->blend[0] = MSG_ReadByte() / 255.0f;
-		to->blend[1] = MSG_ReadByte() / 255.0f;
-		to->blend[2] = MSG_ReadByte() / 255.0f;
-		to->blend[3] = MSG_ReadByte() / 255.0f;
-	}
+    if (flags & PS_BLEND)
+        MSG_ReadBlend(to, psflags);
 
-	if (flags & PS_FOV)
-		to->fov = MSG_ReadByte();
+    if (flags & PS_FOV)
+        to->fov = MSG_ReadByte();
 
-	if (flags & PS_RDFLAGS)
-		to->rdflags = MSG_ReadByte();
+    if (flags & PS_RDFLAGS)
+        to->rdflags = MSG_ReadByte();
 
-	// parse stats
-	if (extraflags & EPS_STATS) {
-		statbits = MSG_ReadLong();
-		for (i = 0; i < MAX_STATS; i++) {
-			if (statbits & (1U << i)) {
-				to->stats[i] = MSG_ReadShort();
-			}
-		}
-	}
-
+    // parse stats
+    if (extraflags & EPS_STATS)
+        MSG_ReadStats(to, psflags);
 }
-
 
 #endif // USE_CLIENT
 
